998e746a28688a02a65ad5d2badada8f5d086606
teddy0 thinkpad laptop <fifadude114@gmail.com>
2022-04-27 16:40:23 -0700

    tried filesystem, appdomain and assembly methods of providing relative filepaths to the vbproj directory, all failed

--- UnitTests.netcore.DWSim.xmlLibs/UnitTest1.vb
+++ UnitTests.netcore.DWSim.xmlLibs/UnitTest1.vb
@@ -6,6 +6,18 @@ Imports netcore.DWSim.xmlLibs
 Imports System.Xml
 Imports System.Xml.Linq
 
+
+'' for Assmebly references
+Imports System.Reflection
+
+
+'' for FileSystem
+Imports Microsoft.VisualBasic.FileIO
+
+
+'' for Directory class
+Imports System.IO
+
 Namespace UnitTests.netcore.DWSim.xmlLibs
     Public Class UnitTest1
 
@@ -17,10 +29,38 @@ Namespace UnitTests.netcore.DWSim.xmlLibs
 		xmlLibLoader = new dwSimXmlLibLoader
 		
 		Dim xmlDoc As XmlDocument
+		
 
-		xmlDoc = xmlLibLoader.getXmlDoc()
+		Dim typeXmlLib As Type
 
-		Console.WriteLine(xmlDoc)
+		typeXmlLib = xmlLibLoader.GetType
+
+		Dim assem As Assembly 
+
+		assem = Assembly.GetAssembly(typeXmlLib)
+
+		Console.WriteLine(assem)
+		Console.WriteLine(assem.GetManiFestResourceStream("DWSIM.Thermodynamics.dwsim.xml"))
+		Console.WriteLine("assembly location")
+		Console.WriteLine(assem.GetExecutingAssembly().Location)
+
+		Dim fileSysObj As FileSystem
+		fileSysObj = new FileSystem
+
+		Console.WriteLine(FileSysObj.CurrentDirectory)
+
+		Console.WriteLine(" ")
+		Console.WriteLine("AppDomain method")
+
+		Dim path as String = AppDomain.CurrentDomain.BaseDirectory
+		Console.WriteLine(path)
+
+
+
+
+		'xmlDoc = xmlLibLoader.getXmlDoc()
+
+		'Console.WriteLine(xmlDoc)
 
 
         End Sub
