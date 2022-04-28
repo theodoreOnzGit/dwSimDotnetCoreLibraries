0d468151c8a49155dede526f747812b3acee0a36
Ted <fifadude114@gmail.com>
2022-04-28 12:09:06 -0700

    filepathloader mockup tests complete

--- UnitTests.netcore.DWSim.xmlLibs/UnitTest1.vb
+++ UnitTests.netcore.DWSim.xmlLibs/UnitTest1.vb
@@ -2,70 +2,61 @@ Imports System
 Imports Xunit
 Imports netcore.DWSim.xmlLibs
 
-
 Imports System.Xml
 Imports System.Xml.Linq
 
 
-'' for Assmebly references
-Imports System.Reflection
-
-
-'' for FileSystem
-Imports Microsoft.VisualBasic.FileIO
-
-
-'' for Directory class
-Imports System.IO
-
 Namespace UnitTests.netcore.DWSim.xmlLibs
+
     Public Class UnitTest1
 
         <Fact>
         Sub TestSub()
 
-		Dim xmlLibLoader As IXmlLibLoader
-		
-		xmlLibLoader = new dwSimXmlLibLoader
-		
-		Dim xmlDoc As XmlDocument
-		
-
-		Dim typeXmlLib As Type
-
-		typeXmlLib = xmlLibLoader.GetType
-
-		Dim assem As Assembly 
-
-		assem = Assembly.GetAssembly(typeXmlLib)
-
-		Console.WriteLine(assem)
-		Console.WriteLine(assem.GetManiFestResourceStream("DWSIM.Thermodynamics.dwsim.xml"))
-		Console.WriteLine("assembly location")
-		Console.WriteLine(assem.GetExecutingAssembly().Location)
-
-		Dim fileSysObj As FileSystem
-		fileSysObj = new FileSystem
-
-		Console.WriteLine(FileSysObj.CurrentDirectory)
-
-		Console.WriteLine(" ")
-		Console.WriteLine("AppDomain method")
-
-		Dim path as String = AppDomain.CurrentDomain.BaseDirectory
-		Console.WriteLine(path)
-
-
-
-
-		'xmlDoc = xmlLibLoader.getXmlDoc()
-
-		'Console.WriteLine(xmlDoc)
-
 
         End Sub
 
 
+		<Fact>
+		Sub TestXmlFileString()
+
+			'' the first part tries the fileSystemFilePath Implementation
+			' there should be a 
+			Dim filePathStringObj As IXmlFilePathString
+			filePathStringObj = new fileSystemFilePathLoader
+
+			Dim resultString As String
+
+			Try
+			    resultString = filePathStringObj.getCurrentFilePath()
+			Catch ex As Exception
+			    Console.WriteLine(ex.Message)
+			End Try
+
+
+			filePathStringObj = Nothing
+
+			'' now for the second implementation
+
+			filePathStringObj = new systemIODirectoryFilePathLoader
+
+			resultString = filePathStringObj.getCurrentFilePath()
+
+			Console.WriteLine(resultString)
+
+
+			'' now for the third implementation
+			
+			filePathStringObj = new assemblyFilePathLoader
+			resultString = filePathStringObj.getCurrentFilePath()
+			Console.WriteLine(resultString)
+
+
+
+
+		End Sub
+
     End Class
+
 End Namespace
 
